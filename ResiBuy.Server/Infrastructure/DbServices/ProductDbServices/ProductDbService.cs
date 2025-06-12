86f81b224f02d40a966e298044d0e07a37d17fe1
DeanAnhDev <dinhtheanhts29012005@gmail.com>
2025-06-12 15:50:23 +0700

    commit api product category

---
+++ ResiBuy.Server/Infrastructure/DbServices/ProductDbServices/ProductDbService.cs
@@ -0,0 +1,75 @@
+﻿using ResiBuy.Server.Infrastructure.Filter;
+
+namespace ResiBuy.Server.Infrastructure.DbServices.ProductDbServices
+{
+    public class ProductDbService : BaseDbService<Product>, IProductDbService
+    {
+        private readonly ResiBuyContext _context;
+        public ProductDbService(ResiBuyContext context) : base(context)
+        {
+            this._context = context;
+        }
+
+        public async Task<PagedResult<Product>> GetAllProducts(int pageNumber, int pageSize)
+        {
+            try
+            {
+                var query = _context.Products.AsQueryable();
+
+                var totalCount = await query.CountAsync();
+                var items = await query
+                    .OrderBy(p => p.Id)
+                    .Skip((pageNumber - 1) * pageSize)
+                    .Take(pageSize)
+                    .ToListAsync();
+
+                return new PagedResult<Product>
+                {
+                    Items = items,
+                    TotalCount = totalCount,
+                    PageNumber = pageNumber,
+                    PageSize = pageSize
+                };
+            }
+            catch (Exception ex)
+            {
+                throw new CustomException(ExceptionErrorCode.RepositoryError, ex.Message);
+            }
+        }
+
+        public async Task<Product> GetByIdAsync(Guid id)
+        {
+            try
+            {
+                var product = await _context.Products.FirstOrDefaultAsync(a => a.Id == id);
+                if (product == null)
+                {
+                    return null;
+                }
+                return product;
+            }
+            catch (Exception ex)
+            {
+                throw new CustomException(ExceptionErrorCode.RepositoryError, ex.Message);
+            }
+        }
+
+        public async Task<Product> GetProductByIdWithStoreAsync(Guid id)
+        {
+            try
+            {
+                var product = await _context.Products.Include(p => p.Store).FirstOrDefaultAsync(a => a.Id == id);
+                if (product == null)
+                {
+                    return null;
+                }
+                return product;
+            }
+            catch (Exception ex)
+            {
+                throw new CustomException(ExceptionErrorCode.RepositoryError, ex.Message);
+            }
+        }
+
+    }
+}
---
+++ ResiBuy.Server/Infrastructure/Model/DTOs/CategoryDto.cs
@@ -0,0 +1,9 @@
+﻿namespace ResiBuy.Server.Infrastructure.Model.DTOs
+{
+    public class CategoryDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Status { get; set; }
+    }
+}
