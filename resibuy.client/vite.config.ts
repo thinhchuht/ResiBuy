349748990a0e79c6f45e888b578ae971cad8b8d4
Quảng Trần Xuân <tranxuanquang20@gmail.com>
2025-06-13 22:52:14 +0700

    xóa css

--- resibuy.client/vite.config.ts
+++ resibuy.client/vite.config.ts
@@ -1,12 +1,12 @@
 import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import plugin from "@vitejs/plugin-react";
-import tailwindcss from '@tailwindcss/vite'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     plugin(),
-    tailwindcss()
+ 
   ],
   resolve: {
     alias: {
