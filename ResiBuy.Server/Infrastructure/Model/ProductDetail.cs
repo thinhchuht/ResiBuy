65d6a04e45286dd397f9e67b4c52b5d6f797c93c
DeanAnhDev <dinhtheanhts29012005@gmail.com>
2025-06-18 22:51:50 +0700

    update base models

--- ResiBuy.Server/Infrastructure/Model/ProductDetail.cs
+++ ResiBuy.Server/Infrastructure/Model/ProductDetail.cs
@@ -8,9 +8,18 @@
         public Product Product { get; set; }
         public int Sold { get; set; }
         public decimal Price { get; set; }
+        public WeightCategory Weight { get; set; }
         public Image Image { get; set; }
         public IEnumerable<CartItem> CartItems { get; set; }
         public IEnumerable<OrderItem> OrderItems { get; set; }
         public IEnumerable<AdditionalData> AdditionalData { get; set; } = new List<AdditionalData>();
     }
+
+    public enum WeightCategory
+    {
+        Light = 1,       // Nhẹ
+        Heavy = 2,       // Nặng
+        VeryHeavy = 3    // Rất nặng
+    }
+
 }
