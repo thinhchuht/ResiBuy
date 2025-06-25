244d3e37beeb6ec935be5060433472f440acb976
thinhld22 <112111530+thinhchuht@users.noreply.github.com>
2025-06-25 21:25:21 +0700

    Chinh dev (#43)
    * test
    
    * Revert "test"
    
    This reverts commit b328b86d55c2e4d865a9ef9b6e9561a19056ddfb.
    
    * db product fix
    
    * fix data name
    
    * add user config, command, update migraiont, usercontroller
    
    * store UI
    
    * fix conflict
    
    * new store
    
    * create product
    
    * storecomit
    
    * add code
    
    ---------
    
    Co-authored-by: Chinh <anhvohoanghuy25@gmail.com>

---
+++ resibuy.client/src/pages/Store/menuItems.ts
@@ -0,0 +1,13 @@
+
+export interface MenuItem {
+  name: string;
+  path: string;
+}
+
+export const menuItems: MenuItem[] = [
+  { name: "Dashboard", path: "/store" },
+  { name: "Products", path: "/store/products" },
+  { name: "Orders", path: "/store/orders" },
+  { name: "CreateProduct", path: "/store/create" }
+];
+
