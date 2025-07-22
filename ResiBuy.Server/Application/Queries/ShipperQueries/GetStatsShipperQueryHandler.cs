45769deeca59e3d69310672196baa83d1c5cf783
Quảng Trần Xuân <tranxuanquang20@gmail.com>
2025-07-22 18:13:34 +0700

    api shipper

---
+++ ResiBuy.Server/Application/Queries/ShipperQueries/GetStatsShipperQueryHandler.cs
@@ -0,0 +1,40 @@
+﻿namespace ResiBuy.Server.Application.Queries.ShipperQueries
+{
+    public record GetStatsShipperQuery() : IRequest<ResponseModel>;
+    public class GetStatsShipperQueryHandler : IRequestHandler<GetStatsShipperQuery, ResponseModel>
+    {
+        private readonly IShipperDbService _shipperDbService;
+
+        public GetStatsShipperQueryHandler(IShipperDbService shipperDbService)
+        {
+            _shipperDbService = shipperDbService;
+        }
+
+        public async Task<ResponseModel> Handle(GetStatsShipperQuery request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var onlineTrue = await _shipperDbService.CountShippersByOnlineStatusAsync(true);
+                var onlineFalse = await _shipperDbService.CountShippersByOnlineStatusAsync(false);
+
+                var shippingTrue = await _shipperDbService.CountShippersByShippingStatusAsync(true);
+                var shippingFalse = await _shipperDbService.CountShippersByShippingStatusAsync(false);
+
+                var totalReportCount = await _shipperDbService.SumShipperReportCountAsync();
+
+                return ResponseModel.SuccessResponse(new
+                {
+                    OnlineTrue = onlineTrue,
+                    OnlineFalse = onlineFalse,
+                    ShippingTrue = shippingTrue,
+                    ShippingFalse = shippingFalse,
+                    TotalReportCount = totalReportCount
+                });
+            }
+            catch (Exception ex)
+            {
+                throw new CustomException(ExceptionErrorCode.RepositoryError, ex.Message);
+            }
+        }
+    }
+}
