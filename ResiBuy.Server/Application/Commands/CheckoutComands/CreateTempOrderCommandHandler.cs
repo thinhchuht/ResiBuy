de4ec60fc6efe17db0586141c80d882f926953c4
thinhchuht <thinhchuhtt@gmail.com>
2025-08-02 16:11:24 +0700

    uncomment shipping fee

--- ResiBuy.Server/Application/Commands/CheckoutComands/CreateTempOrderCommandHandler.cs
+++ ResiBuy.Server/Application/Commands/CheckoutComands/CreateTempOrderCommandHandler.cs
@@ -19,20 +19,20 @@ namespace ResiBuy.Server.Application.Commands.CheckoutComands
                 throw new CustomException(ExceptionErrorCode.ValidationFailed, "Chỉ có người mua hàng mới tạo được đơn hàng");
             var db = redisService.GetDatabase();
             var groupedCartItems = dto.CartItems.GroupBy(ci => ci.StoreId);
-            if(groupedCartItems.Count() > 10) throw new CustomException(ExceptionErrorCode.ValidationFailed, "Không thể tạo quá 10 đơn hàng 1 lần");
+            if (groupedCartItems.Count() > 10) throw new CustomException(ExceptionErrorCode.ValidationFailed, "Không thể tạo quá 10 đơn hàng 1 lần");
             var tempOrders = new List<TempOrderDto>();
             foreach (var group in groupedCartItems)
             {
                 var storeId = group.Key;
                 var store = await storeDbService.GetStoreByIdAsync(storeId);
-               if(store.OwnerId == user.Id) throw new CustomException(ExceptionErrorCode.ValidationFailed, "Không thể mua hàng từ chính cửa hàng của mình");
+                if (store.OwnerId == user.Id) throw new CustomException(ExceptionErrorCode.ValidationFailed, "Không thể mua hàng từ chính cửa hàng của mình");
                 var tempProductDetails = new List<TempProductDetailDto>();
                 foreach (var ci in group)
                 {
                     var productDetail = await productDetailDbService.GetByIdAsync(ci.ProductDetailId);
                     if (productDetail == null)
                         throw new CustomException(ExceptionErrorCode.NotFound, $"Không tìm thấy sản phẩm: {ci.ProductDetailId}");
-                    if(productDetail.IsOutOfStock || productDetail.Quantity <= 0) throw new CustomException(ExceptionErrorCode.NotFound, $"Sản phẩm {productDetail.Product.Name} đã hết hàng");
+                    if (productDetail.IsOutOfStock || productDetail.Quantity <= 0) throw new CustomException(ExceptionErrorCode.NotFound, $"Sản phẩm {productDetail.Product.Name} đã hết hàng");
                     if (productDetail.Quantity < ci.Quantity)
                         throw new CustomException(ExceptionErrorCode.ValidationFailed, $"Mặt hàng {productDetail.Product.Name} chỉ còn {productDetail.Quantity} sản phẩm có sẵn");
                     tempProductDetails.Add(new TempProductDetailDto(
@@ -46,8 +46,8 @@ namespace ResiBuy.Server.Application.Commands.CheckoutComands
                         productDetail.AdditionalData.Select(ad => { ad.ProductDetail = null; return ad; }).ToList()
                     ));
                 }
-                //var shippingFee = await orderDbService.ShippingFeeCharged(user.UserRooms.First().RoomId, store.RoomId, tempProductDetails.Sum(ci => ci.Weight));
-                var totalPrice = tempProductDetails.Sum(x => x.Price * x.Quantity) + 5000;
+                var shippingFee = await orderDbService.ShippingFeeCharged(user.UserRooms.First().RoomId, store.RoomId, tempProductDetails.Sum(ci => ci.Weight));
+                var totalPrice = tempProductDetails.Sum(x => x.Price * x.Quantity) + shippingFee;
                 var tempOrder = new TempOrderDto
                 {
                     Id = Guid.NewGuid(),
@@ -55,7 +55,7 @@ namespace ResiBuy.Server.Application.Commands.CheckoutComands
                     TotalBeforeDiscount = Math.Round(tempProductDetails.Sum(x => x.Price * x.Quantity), MidpointRounding.AwayFromZero),
                     TotalPrice = Math.Round(tempProductDetails.Sum(x => x.Price * x.Quantity), MidpointRounding.AwayFromZero),
                     ProductDetails = tempProductDetails,
-                    ShippingFee = 5000,
+                    ShippingFee = shippingFee,
                 };
                 tempOrders.Add(tempOrder);
             }
