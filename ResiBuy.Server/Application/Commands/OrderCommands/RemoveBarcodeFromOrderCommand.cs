698827a17038f07ac5b53bd28c99bbe092622dad
Chinh <anhvohoanghuy25@gmail.com>
2025-09-25 20:55:25 +0700

    api key open route

--- ResiBuy.Server/Application/Commands/OrderCommands/RemoveBarcodeFromOrderCommand.cs
+++ ResiBuy.Server/Application/Commands/OrderCommands/RemoveBarcodeFromOrderCommand.cs
@@ -26,7 +26,7 @@ namespace ResiBuy.Server.Application.Commands.OrderCommands
                 throw new CustomException(ExceptionErrorCode.ValidationFailed, $"Mã vạch {request.dto.BarcodeToRemove} không có trong order nào");
             else if (barcode.OrderItem.Order.Status != OrderStatus.Delivered)
                 throw new CustomException(ExceptionErrorCode.ValidationFailed, $"Mã vạch {request.dto.BarcodeToRemove} chỉ có thể xóa khỏi đơn hàng đã giao");
-
+            var price = barcode.OrderItem.Price;
             barcode.OrderItem.Quantity -= 1;
             barcode.OrderItem.Order.IsReport = true;
             barcode.OrderItem.Order.TotalPrice -= barcode.OrderItem.Price;
@@ -45,7 +45,7 @@ namespace ResiBuy.Server.Application.Commands.OrderCommands
             {
                 throw new CustomException(ExceptionErrorCode.UpdateFailed, ex.Message);
             }
-            return ResponseModel.SuccessResponse($"Đã xóa {barcode.Code} mã vạch khỏi order .Giá trị sản phẩm: {barcode.OrderItem.Price}");
+            return ResponseModel.SuccessResponse($"Đã xóa {barcode.Code} mã vạch khỏi order .Giá trị sản phẩm: {price}");
         }
     }
 }
