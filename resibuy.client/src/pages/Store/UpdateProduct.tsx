6b78afac081178ef309e9545235abd020eb5582b
Chinh <anhvohoanghuy25@gmail.com>
2025-09-26 03:56:31 +0700

    fix fix

--- resibuy.client/src/pages/Store/UpdateProduct.tsx
+++ resibuy.client/src/pages/Store/UpdateProduct.tsx
@@ -614,7 +614,7 @@ export default function UpdateProduct() {
 
         allDetails.forEach((detail, index) => {
             if (detail.price <= 0 || !Number.isInteger(detail.price)) {
-                newPriceErrors[index] = "Giá phải lớn hơn 0 và là bội số 500";
+                newPriceErrors[index] = "Giá phải lớn hơn 0";
                 isValid = false;
             }
 
@@ -662,8 +662,6 @@ export default function UpdateProduct() {
 
         if (price <= 0) {
             newErrors[index] = "Giá phải lớn hơn 0";
-        }else if (price%500 !==0)  {
-            newErrors[index] = "Giá phải là bội số của 500";
         }else {
             delete newErrors[index];
         }
